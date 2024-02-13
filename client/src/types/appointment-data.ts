484990779a3e00ed215a7a6259d254a4aac90a7f
Thiago Souto <thiagosouto.contato@gmail.com>
2024-02-13 13:33:04 -0300

    Client: add entities data type definitions

---
+++ client/src/types/appointment-data.ts
@@ -0,0 +1,8 @@
+export type AppointmentData = {
+  appointmentUId: string;
+  patientCPF: string;
+  patientFullName: string;
+  type: string;
+  bookingDate: string;
+  bookingTime: string;
+};
---
+++ client/src/types/patient-data.ts
@@ -0,0 +1,7 @@
+export type PatientData = {
+  patientUId: string;
+  cpf: string;
+  fullName: string;
+  birthday: string;
+  sex: 'M' | 'F';
+};
