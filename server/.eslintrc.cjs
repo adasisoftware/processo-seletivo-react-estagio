2924824fe777ef0d8dd640bc6cd70538b05e6c2f
Thiago Souto <thiagosouto.contato@gmail.com>
2024-02-13 16:08:36 -0300

    Server: add eslint config file

---
+++ server/.eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['*./tsconfig.json'],
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
